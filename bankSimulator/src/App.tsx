b47128a363c25b748459c18d8ab3e6655ed5634c
tobias odion <tobiasodion@gmail.com>
2024-06-19 09:45:39 +0200

    Initialize bank simulator app

---
+++ bankSimulator/src/App.tsx
@@ -0,0 +1,14 @@
+function App() {
+  return (
+    <div>
+      <div>Bank Simulator</div>
+      <div className="flex justify-between">
+        <div className="w-[50vw]">API</div>
+        <div className="w-full">Description</div>
+        <div className="w-full">Simulator</div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
