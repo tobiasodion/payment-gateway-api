4d80e0e4d150dc3bf2107593e3c6e1b5f14454be
tobias odion <tobiasodion@gmail.com>
2024-06-19 16:51:37 +0200

    Add validation to simulator input

--- bankSimulator/src/App.tsx
+++ bankSimulator/src/App.tsx
@@ -1,13 +1,64 @@
 import { useEffect, useState } from "react";
-import { getPaymentSimulation } from "./data/simulations";
+import {
+  AcquirerMode,
+  SimulationType,
+  simulationsData,
+} from "./data/simulations";
 import { apis } from "./data/simulations";
 
 function App() {
   const [idInput, setIdInput] = useState("");
   const [id, setId] = useState<string>();
+  const [payloadInput, setPayloadInput] = useState<string>("");
+  const [payload, setPayload] = useState<string>();
   const [response, setResponse] = useState<{ code: number; body: string }>();
+  const [simulation, setSimulation] = useState(simulationsData[0]);
 
   const [loading, setLoading] = useState(false);
+  const [isChecked, setIsChecked] = useState(false);
+  const [isValidInput, setIsValidInput] = useState(false);
+
+  const handleCheckboxChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setIsChecked(event.target.checked);
+  };
+
+  const handleSubmit = (event: React.FormEvent) => {
+    event.preventDefault();
+    if (simulation.type === SimulationType.GET_PAYMENT_BY_ID) {
+      setId(idInput);
+    }
+    if (simulation.type === SimulationType.POST_PAYMENT) {
+      setPayload(payloadInput);
+    }
+  };
+
+  const handleApiChange = (apiId: number) => {
+    const selectedSimulation = simulationsData.find(
+      (simulation) => simulation.id === apiId
+    );
+    if (selectedSimulation) {
+      setPayloadInput("");
+      setIdInput("");
+      setIsValidInput(false);
+      setResponse(undefined);
+      setSimulation(selectedSimulation);
+    }
+  };
+
+  const validateInput = (input: string) => {
+    if (simulation.type === SimulationType.GET_PAYMENT_BY_ID) {
+      setIsValidInput(input.length > 0);
+    }
+
+    if (simulation.type === SimulationType.POST_PAYMENT) {
+      try {
+        JSON.parse(input);
+        setIsValidInput(input.length > 0);
+      } catch (error) {
+        setIsValidInput(false);
+      }
+    }
+  };
 
   useEffect(() => {
     const fetchPayment = async () => {
@@ -34,42 +85,90 @@ function App() {
     id && fetchPayment();
   }, [id]);
 
-  const handleSubmit = (event: React.FormEvent) => {
-    event.preventDefault();
-    setId(idInput);
-  };
+  useEffect(() => {
+    const postPayment = async () => {
+      try {
+        setLoading(true);
+        const response = await fetch(`http://localhost:3000/payments/`, {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+            "x-acquirer-Mode": isChecked
+              ? AcquirerMode.FAILURE
+              : AcquirerMode.SUCCESS,
+          },
+          body: payload,
+        });
+        const data = await response.json();
+        setResponse({
+          code: response.status,
+          body: JSON.stringify(data, null, 2),
+        });
+      } catch (error) {
+        console.error(error);
+        setLoading(false);
+      } finally {
+        setLoading(false);
+      }
+    };
+    payload && postPayment();
+  }, [payload]);
+
   return (
     <div>
-      <div>Bank Simulator</div>
-      <div className="flex justify-between">
-        <div className="w-[50vw]">
-          <h1>API</h1>
+      <div className="font-bold text-2xl p-4 bg-black/90  text-white">
+        Bank Simulator
+      </div>
+      <div className="flex flex-col lg:flex-row justify-between gap-4 p-8">
+        <div className="w-full lg:w-[50vw]  bg-gray-100 rounded-2xl">
+          <h1 className="p-4 border-b-4 font-bold text-lg">API</h1>
           <div className="flex flex-col p-4 gap-4">
             {apis.map((api, idx) => (
-              <div key={idx}>
-                <p>{api.name}</p>
-              </div>
+              <button
+                className="flex items-center justify-between bg-white p-2 rounded-xl"
+                key={idx}
+                onClick={() => handleApiChange(api.id)}
+              >
+                <p className="font-medium">{api.name}</p>
+                <span className="p-2 rounded-xl text-sm font-medium bg-blue-400 text-white">
+                  {api.method}
+                </span>
+              </button>
             ))}
           </div>
         </div>
-        <div className="w-full flex flex-col">
-          <h1>Description</h1>
+        <div className="w-full flex flex-col bg-gray-100 rounded-2xl">
+          <h1 className="p-4 border-b-4 font-bold text-lg">Description</h1>
+
           <div className="p-4">
-            <p>{getPaymentSimulation.name}</p>
-            <p>{getPaymentSimulation.description}</p>
+            <span className="flex  pb-4 font-semibold text-xl">
+              {simulation.type}
+            </span>
+            <p className="pb-4 italic">{simulation.description}</p>
             <div className="flex justify-between">
-              <p>{getPaymentSimulation.endpoint}</p>
-              <p>{getPaymentSimulation.method}</p>
+              <p>
+                <span className="font-medium">Endpoint:</span>{" "}
+                {simulation.endpoint}
+              </p>
+              <span className="p-2 rounded-xl text-sm font-medium bg-blue-400 text-white">
+                {simulation.method}
+              </span>
             </div>
             <div>
-              <div>Simulation Data:</div>
+              <div className="pb-4 font-medium">Simulation Data:</div>
               <div>
-                {getPaymentSimulation.test.map((test, idx) => (
-                  <div key={idx}>
-                    <p>{test.name}</p>
-                    <ul>
+                {simulation.test.map((test, idx) => (
+                  <div className="pb-2" key={idx}>
+                    <p className="font-medium pb-2 border-b-4">{test.name}:</p>
+                    <ul className="py-2 px-4 bg-white overflow-scroll">
                       {test.values.map((value, idx) => (
-                        <li key={idx}>{value}</li>
+                        <li key={idx}>
+                          <pre>
+                            {typeof value === "string"
+                              ? value
+                              : JSON.stringify(value, null, 2)}
+                          </pre>
+                        </li>
                       ))}
                     </ul>
                   </div>
@@ -77,55 +176,117 @@ function App() {
               </div>
             </div>
             <div>
-              <div>Responses:</div>
-              <div>
-                {getPaymentSimulation.response.map((response, idx) => (
-                  <div key={idx}>
-                    <div className="flex justify-between">
-                      <p>{response.description}</p>
+              <div className="font-medium pb-2">Example Responses:</div>
+              <div className="font-medium">
+                {simulation.response.map((response, idx) => (
+                  <div className="pb-2" key={idx}>
+                    <div className="flex justify-between border-b-4 p-2">
+                      <p>{response.name}</p>
                       <p>{response.status}</p>
                     </div>
-                    <pre>{JSON.stringify(response.body, null, 2)}</pre>
+                    <p className="p-2">{response.description}</p>
+                    <pre className="pt-4 bg-white p-4 overflow-scroll">
+                      {JSON.stringify(response.body, null, 2)}
+                    </pre>
                   </div>
                 ))}
               </div>
             </div>
           </div>
         </div>
-        <div className="w-full">
-          <h1>Simulator</h1>
+        <div className="w-full bg-gray-100 rounded-2xl">
+          <h1 className="p-4 border-b-4 font-bold text-lg">Simulator</h1>
           <div className="flex flex-col h-[100vh]">
             <div className="p-4 h-full flex-1">
               <form onSubmit={handleSubmit}>
                 <div className="flex flex-col w-full justify-between gap-4">
-                  <div className="flex w-full">
-                    <label className="w-[10%]" htmlFor="id">
-                      Id
-                    </label>
-                    <input
-                      className="w-[90%]"
-                      type="text"
-                      id="id"
-                      value={idInput}
-                      onChange={(e) => setIdInput(e.target.value)}
-                    />
-                  </div>
+                  {simulation.type === SimulationType.GET_PAYMENT_BY_ID && (
+                    <div className="flex w-full">
+                      <label
+                        className="flex w-[10%] h-full p-2 items-center font-semibold"
+                        htmlFor="id"
+                      >
+                        Id
+                      </label>
+                      <input
+                        className="w-[90%] p-2 overflow-scroll"
+                        type="text"
+                        id="id"
+                        value={idInput}
+                        onChange={(e) => {
+                          validateInput(e.target.value);
+                          setIdInput(e.target.value);
+                        }}
+                      />
+                    </div>
+                  )}
+                  {simulation.type === SimulationType.POST_PAYMENT && (
+                    <div className="flex w-full flex-col">
+                      <div className="flex justify-between">
+                        <label
+                          className="flex h-full p-2 items-center font-semibold"
+                          htmlFor="id"
+                        >
+                          Payload
+                        </label>
+                        <div className="flex justify-center items-center">
+                          <label
+                            className="flex h-full p-2 items-center font-semibold"
+                            htmlFor="id"
+                          >
+                            Simulate Acquirer Fail
+                          </label>
+                          <input
+                            className="w-6 h-6"
+                            type="checkbox"
+                            checked={isChecked}
+                            onChange={handleCheckboxChange}
+                          />
+                        </div>
+                      </div>
+                      <textarea
+                        className="p-4 h-[30vh] overflow-scroll flex"
+                        id="id"
+                        value={payloadInput}
+                        onChange={(e) => {
+                          setPayloadInput(e.target.value);
+                          validateInput(e.target.value);
+                        }}
+                      />
+                    </div>
+                  )}
 
-                  <button type="submit">Submit</button>
+                  <button
+                    className={`rounded-sm ${
+                      isValidInput ? "bg-blue-500" : "bg-gray-400"
+                    }  h-8 text-white`}
+                    type="submit"
+                    disabled={!isValidInput}
+                  >
+                    Submit
+                  </button>
+                  {!isValidInput &&
+                    simulation.type === SimulationType.POST_PAYMENT && (
+                      <span className="italic">
+                        Ensure input is valid JSON string
+                      </span>
+                    )}
                 </div>
               </form>
             </div>
             <div className="p-4 h-full justify-center items-center">
               <div className="flex justify-between">
-                <div>Response</div>
+                <div className="font-semibold pb-2">Response</div>
                 {response && <div>Status Code: {response?.code}</div>}
               </div>
 
-              <div className="p-4 justify-center items-center">
+              <div className="flex justify-center bg-white h-full">
                 {loading ? (
                   <span>Loading...</span>
                 ) : (
-                  <pre>{response?.body}</pre>
+                  <pre className="p-4 overflow-scroll text-wrap">
+                    {response?.body}
+                  </pre>
                 )}
               </div>
             </div>
--- bankSimulator/src/data/simulations.ts
+++ bankSimulator/src/data/simulations.ts
@@ -1,53 +1,177 @@
 export const apis = [
-  { id: 1, name: "GetPaymentById" },
-  { id: 2, name: "PostPayment" },
+  { id: 1, name: "GetPaymentById", method: "GET", endpoint: "/payments/{id}" },
+  { id: 2, name: "PostPayment", method: "POST", endpoint: "/payments" },
 ];
 
-export const getPaymentSimulation = {
-  id: "1",
-  name: "GetPaymentById",
-  description:
-    "Retrieve details of a previously made payment using the payment Id",
-  method: "GET",
-  endpoint: "/payments/{id}",
-  test: [
-    {
-      name: "Test Ids",
-      values: [
-        "9b4f5f0a-6f1d-4f99-9d13-5b35cfad6dbf",
-        "3f98fbbc-b5f1-4b7b-824b-4c6a7ddf1a0a",
-        "d230cf92-ec5b-4828-8f62-5b43a65c017d",
-        "cb1d81e7-8f64-42ed-a682-83e9eae0c26e",
-      ],
-    },
-  ],
-  response: [
-    {
-      status: 200,
-      description: "Payment found",
-      body: {
-        id: "d446e2b3-0977-4f1c-9d2f-5a1d6c781f63",
-        amount: 100,
-        currency: "USD",
-        status: "AUTHORIZED",
-        merchantId: "1",
-        creditCard: {
-          cardNumber: "1234-5678-1234-5678",
-          cardHolder: "John Doe",
-          expirationDate: "10/22",
-          cvv: "123",
+export enum SimulationType {
+  GET_PAYMENT_BY_ID = "GetPaymentById",
+  POST_PAYMENT = "PostPayment",
+}
+
+export enum AcquirerMode {
+  SUCCESS = "success",
+  FAILURE = "failure",
+}
+
+export const simulationsData: {
+  id: number;
+  type: SimulationType;
+  description: string;
+  method: string;
+  endpoint: string;
+  test: {
+    name: string;
+    values: string[] | object[];
+  }[];
+  response: {
+    status: number;
+    name: string;
+    description: string;
+    body: Record<string, unknown>;
+  }[];
+}[] = [
+  {
+    id: 1,
+    type: SimulationType.GET_PAYMENT_BY_ID,
+    description:
+      "Retrieve details of a previously made payment using the payment Id. The sensitive card details(card number and cvv) are masked in the response.",
+    method: "GET",
+    endpoint: "/payments/{id}",
+    test: [
+      {
+        name: "Test Ids",
+        values: [
+          "9b4f5f0a-6f1d-4f99-9d13-5b35cfad6dbf",
+          "3f98fbbc-b5f1-4b7b-824b-4c6a7ddf1a0a",
+          "d230cf92-ec5b-4828-8f62-5b43a65c017d",
+          "cb1d81e7-8f64-42ed-a682-83e9eae0c26e",
+        ],
+      },
+    ],
+    response: [
+      {
+        status: 200,
+        name: "Payment found",
+        description: "Payment details found for the provided Id",
+        body: {
+          id: "cb1d81e7-8f64-42ed-a682-83e9eae0c26e",
+          amount: 400,
+          currency: "USD",
+          status: "declined",
+          merchantId: "2",
+          creditCard: {
+            cardNumber: "**** **** **** 5678",
+            cardHolder: "John Doe",
+            expirationDate: "10/22",
+            cvv: "****",
+          },
+          reference: "ref-101",
         },
-        reference: "ref-123",
       },
-    },
-    {
-      status: 404,
-      description: "Payment not found",
-      body: {
-        requestId: "1395ce51-5f62-456c-b5c8-36840a4e2989",
-        errorType: "NotFoundError",
-        errorMessage: "Payment not found",
+      {
+        status: 404,
+        name: "Payment not found",
+        description: "Payment details found for the provided Id",
+        body: {
+          requestId: "1395ce51-5f62-456c-b5c8-36840a4e2989",
+          errorType: "NotFoundError",
+          errorMessage: "Payment not found",
+        },
       },
-    },
-  ],
-};
+    ],
+  },
+  {
+    id: 2,
+    type: SimulationType.POST_PAYMENT,
+    description:
+      "Create a new payment with the provided details, sends the payment to the acquirer for processing and returns the status of the payment",
+    method: "POST",
+    endpoint: "/payments",
+    test: [
+      {
+        name: "Test Merchant Ids",
+        values: ["01", "02", "03"],
+      },
+      {
+        name: "Test payload",
+        values: [
+          {
+            amount: 1000,
+            currency: "USD",
+            merchantId: "01",
+            creditCard: {
+              cardNumber: "1234-5678-9012-3457",
+              cardHolder: "John Doe",
+              expirationDate: "06/24",
+              cvv: "123",
+            },
+            reference: "123456",
+          },
+        ],
+      },
+    ],
+    response: [
+      {
+        status: 200,
+        name: "Payment authorized",
+        description:
+          "The payment processing by the acquiring bank was successful",
+        body: {
+          id: "93aed40b-c648-4a4a-b7a2-ef7ba6e36d65",
+          status: "authorized",
+          amount: 1000,
+          currency: "USD",
+          merchantId: "01",
+          creditCard: {
+            cardNumber: "**** **** **** 3457",
+            cardHolder: "John Doe",
+            expirationDate: "06/24",
+            cvv: "****",
+          },
+          reference: "123456",
+        },
+      },
+      {
+        status: 200,
+        name: "Payment declined",
+        description:
+          "The payment processing by the acquiring bank was unsuccessful",
+        body: {
+          id: "d6c4a187-7ae6-4352-82b5-ab26bec8831c",
+          status: "declined",
+          amount: 1000,
+          currency: "USD",
+          merchantId: "01",
+          creditCard: {
+            cardNumber: "**** **** **** 3457",
+            cardHolder: "John Doe",
+            expirationDate: "06/24",
+            cvv: "****",
+          },
+          reference: "123456",
+        },
+      },
+      {
+        status: 400,
+        name: "Invalid merchant Id",
+        description: "The merchant Id provided does not exist on the system",
+        body: {
+          requestId: "2a54ea2f-e1cc-4d32-9f05-6110a4632fe9",
+          errorType: "InvalidMerchantError",
+          errorMessage: "Invalid merchant Id",
+        },
+      },
+      {
+        status: 400,
+        name: "Invalid Request",
+        description:
+          "Including amount less than or equal to 0, unsupported/invalid currency, invalid card number format, invalid carholder format, invalid expiration date format, expired card, invalid cvv, inalid reference format",
+        body: {
+          requestId: "265ae599-7db5-4945-807b-d5a2821f1ec4",
+          errorType: "BadRequestError",
+          errorMessage: "<Appropriate error message>",
+        },
+      },
+    ],
+  },
+];
